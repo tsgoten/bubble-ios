2d12275b97d627dce54254158f56e202dc8abea5
Tarang <tsgoten@gmail.com>
2019-05-07 12:45:11 -0400

    Current location function is still WIP

--- Bubble/Bubble/HomeViewController.swift
+++ Bubble/Bubble/HomeViewController.swift
@@ -11,10 +11,21 @@ import CoreLocation
 
 class HomeViewController: UIViewController {
     
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.setCenter(currentLocation, animated: true)
+        //mapView.setCenter(currentLocation, animated: true)
         // Do any additional setup after loading the view, typically from a nib.
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self as? CLLocationManagerDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            mapView.setCenter((locationManager.location?.coordinate)!, animated: true)
+        }
+        
+        mapView.setUserTrackingMode(MKUserTrackingMode.follow, animated: true)
     }
     
     @IBOutlet weak var mapView: MKMapView!
